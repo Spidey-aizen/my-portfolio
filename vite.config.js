87f08e086a8f7c753f4429059a9e83d289e47ce3
aizen <prasannaaizen@gmail.com>
2026-01-22 18:53:22 +0530

    Fix: updated vite base for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/", // Replace this with your GitHub repository name
+  base: '/my-portfolio/', // REPLACE this with your exact repository name
 })
